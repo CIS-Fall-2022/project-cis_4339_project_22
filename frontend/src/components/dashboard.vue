4b32d9049396bffbfd6da69e9ef587ecd94e5e72
Jeremiah Rodriguez <Jeremiahrodriguez37@yahoo.com>
2022-11-13 10:11:22 -0600

    dashboard - pulling data for chart

--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -3,14 +3,41 @@
     <div>
       <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">Welcome</h1>
     </div>
+    <div>
+      <p v-for="query in queryData" :key="query._id"> {{"Attendees: " + query.total}}</p>
+    </div>
   </main>
 </template>
 <script>
+import axios from "axios";
+
+
+// export default {
+//   methods: {
+//     routePush(routeName) {
+//       this.$router.push({ name: routeName });
+//     },
+//   },
+// };
+
 export default {
-  methods: {
-    routePush(routeName) {
-      this.$router.push({ name: routeName });
-    },
+  data() {
+    return {
+      queryData: [],
+      //Parameter for search to occur
+      _id: "",
+      total: "",
+    };
   },
-};
+  mounted() {
+    let apiURL = 'http://localhost:3000/eventdata/eventAttendees';
+    axios.get(apiURL).then((resp) => {
+      this.queryData = resp.data;
+    });
+    window.scrollTo(0, 0);
+  }
+}
+
+
+
 </script>
